47861ca8608c3e512c7bd22c3442671ad7a44aab
Akash Makam <makamakash11@gmail.com>
2024-08-12 21:05:12 +0530

    .

--- Test/5.c
+++ Test/5.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int count = 0;
 
@@ -47,31 +48,33 @@ void merge(int *leftArray, int leftSize, int *rightArray, int rightSize, int *ar
 void worstCaseArrayGenerator(int *array, int n) {
     if (n <= 1) {
         return;
-    }
+    } else {
+        int middle = n / 2;
+        int *leftArray = (int *)malloc(middle * sizeof(int));
+        int *rightArray = (int *)malloc((n - middle) * sizeof(int));
 
-    int middle = n / 2;
-    int *leftArray = (int *)malloc(middle * sizeof(int));
-    int *rightArray = (int *)malloc((n - middle) * sizeof(int));
+        for (int i = 0; i < middle; i++) {
+            leftArray[i] = array[2 * i];
+        }
+        for (int j = 0; j < (n - middle); j++) {
+            rightArray[j] = array[(2 * j) + 1];
+        }
 
-    for (int i = 0; i < middle; i++) {
-        leftArray[i] = array[2 * i];
-    }
-    for (int j = 0; j < (n - middle); j++) {
-        rightArray[j] = array[(2 * j) + 1];
-    }
+        worstCaseArrayGenerator(leftArray, middle);
+        worstCaseArrayGenerator(rightArray, n - middle);
 
-    worstCaseArrayGenerator(leftArray, middle);
-    worstCaseArrayGenerator(rightArray, n - middle);
+        for (int i = 0; i < middle; i++) {
+            count++;
+            array[i] = leftArray[i];
+        }
+        for (int j = 0; j < (n - middle); j++) {
+            count++;
+            array[j + middle] = rightArray[j];
+        }
 
-    for (int i = 0; i < middle; i++) {
-        array[i] = leftArray[i];
+        free(leftArray);
+        free(rightArray);
     }
-    for (int j = 0; j < (n - middle); j++) {
-        array[j + middle] = rightArray[j];
-    }
-
-    free(leftArray);
-    free(rightArray);
 }
 
 void mergeSort(int *array, int n) {
@@ -98,12 +101,59 @@ void mergeSort(int *array, int n) {
     }
 }
 
-int main() {
+
+void plotter() {
+    srand(time(NULL));
+    FILE *bestCase, *worstCase;
+
+    bestCase = fopen("mergeBest.txt", "w");
+    worstCase = fopen("mergeWorst.txt", "w");
+
+    for (int n = 2; n <= 1024; n = n * 2) {
+        int *array = (int *)malloc(n * sizeof(int));
+        
+        // Best case
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            array[i] = i + 1;
+        }
+        mergeSort(array, n);
+        fprintf(bestCase, "%d\t%d\n", n, count);
+
+        // Worst case
+        count = 0;
+        worstCaseArrayGenerator(array, n);
+        fprintf(worstCase, "%d\t%d\n", n, count);
+
+        free(array);
+    }
+
+    fclose(bestCase);
+    fclose(worstCase);
+}
+
+void tester() {
     int n;
     int *array = read(&n);
     mergeSort(array, n);
     display(array, n);
     printf("\nThis took %d comparisons.\n", count);
     free(array);
+    return 0;
+}
+
+int main() {
+    int option;
+    printf("\nChoose any one of the following:\n"
+    "1. Tester\n"
+    "2. Plotter\n"
+    "3. Exit\n");
+    scanf("%d", &option);
+    switch(option) {
+        case 1: tester(); break;
+        case 2: plotter(); break;
+        case 3: exit(0);
+        default: printf("\nInvalid option!\n");
+    }
     return 0;
 }
